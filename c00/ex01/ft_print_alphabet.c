086e7c165a52a2ea17cc86ddec54a2f04dbb6b1d
Mei <108127796+Pheniasse@users.noreply.github.com>
2022-07-12 23:22:57 +0200

    don't forget

--- c00/ex01/ft_print_alphabet.c
+++ c00/ex01/ft_print_alphabet.c
@@ -14,5 +14,5 @@
 
 void ft_print_alphabet(void);
 {
-    write(1, abcdefghijklmonpqrstuvwxyz, 26);
+    write(1, "abcdefghijklmonpqrstuvwxyz", 26);
 }
--- c00/ex04/ft_is_negative.c
+++ c00/ex04/ft_is_negative.c
@@ -15,9 +15,17 @@
 void ft_putchar(char c)
 {
     write(1, &c, 1);
+    return(0);
 }
 
 void ft_is_negative(int n)
 {
-    ft_putchar((n < 0) ? 'N' : 'P');
+    if (n >= 0)
+    {
+        ft_putchar('P');
+    }
+        else
+     {
+         ft_putchar('N')
+     }
 }
